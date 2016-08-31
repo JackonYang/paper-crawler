a29fd8029b3bb9c6b43e64b1d351a6a39f63a9ae
Jackon Yang <i@jackon.me>
2016-08-31 22:32:49 +0800

    支持循环抓取

--- list.py
+++ list.py
@@ -1,9 +1,12 @@
-__author__ = 'huposeya'
 # -*- coding:utf-8 -*-
-#用于获取下载列表
+__author__ = 'huposeya'
+# 用于获取下载列表
 
+import time
 import requests
 from bs4 import BeautifulSoup
+
+import paper
 
 def geturllist(urllist):
     responselist = requests.get(urllist)
@@ -11,19 +14,23 @@ def geturllist(urllist):
     urldata = soup.find_all('span', class_="list-identifier")
     list = []
     for i in urldata:
-        print('http://arxiv.org/' + i('a')[0]['href'])
+
+        url = 'http://arxiv.org/' + i('a')[0]['href']
+
+        print 'downloading url: %s' % url
+        paper_page = paper.papercrawler(url)
+        paper_page.downloadpdf()
+
+        print 'sleeping 5s...'
+        time.sleep(5)
+
         list.append('http://arxiv.org/' + i('a')[0]['href'])
     return list
 
 
-geturllist('http://arxiv.org/list/cs/recent')
+def main(url):
+    geturllist(url)
 
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    url = 'http://arxiv.org/list/cs/recent'
+    main(url)
